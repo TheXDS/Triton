a942cfab03897e0cf0069f40b46c58424b295b46
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2026-02-19 15:53:54 -0600

    Slightly increased coverage

--- src/Tests/Transport/Triton.Tests.InMemory/InMemoryCrudTransactionTests.cs
+++ src/Tests/Transport/Triton.Tests.InMemory/InMemoryCrudTransactionTests.cs
@@ -12,6 +12,26 @@ namespace TheXDS.Triton.Tests.InMemory;
 public class InMemoryCrudTransactionTests
 {
     [Test]
+    public void All_T_gets_all_entities_of_model()
+    {
+        User user = new() { Id = "abc123" };
+        Post post = new() { Id = 1234, Author = user };
+        using var transaction = new InMemoryCrudTransaction([user, post]);
+        var result = transaction.All<User>();
+        Assert.That(result.Result, Is.EquivalentTo([user]));
+    }
+
+    [Test]
+    public void All_gets_all_entities_of_model()
+    {
+        User user = new() { Id = "abc123" };
+        Post post = new() { Id = 1234, Author = user };
+        using var transaction = new InMemoryCrudTransaction([user, post]);
+        var result = transaction.All(typeof(User));
+        Assert.That(result.Result, Is.EquivalentTo([user]));
+    }
+
+    [Test]
     public void Read_reads_entities()
     {
         User user = new() { Id = "abc123" };
@@ -129,6 +149,85 @@ public class InMemoryCrudTransactionTests
         var result = transaction.Delete<User>("abc123");
         Assert.That(result.IsSuccessful, Is.True);
         Assert.That((await transaction.CommitAsync()).IsSuccessful, Is.True);
+        Assert.That(store, Is.Empty);
+    }
+
+    [Test]
+    public async Task Delete_with_non_generic_model_deletes_entities()
+    {
+        User user = new() { Id = "abc123" };
+        User user2 = new() { Id = "abc456" };
+        Post post = new() { Id = 1234, Author = user };
+        ICollection<Model> store = [user, post, user2];
+        using var transaction = new InMemoryCrudTransaction(store);
+        var result = transaction.Delete([user, post]);
+        await transaction.CommitAsync();
+        Assert.That(result.IsSuccessful, Is.True);
+        Assert.That(store, Is.EquivalentTo([user2]));
+    }
+
+
+    [Test]
+    public void Delete_with_non_generic_model_fails_if_not_found()
+    {
+        User user = new() { Id = "abc123" };
+        User user2 = new() { Id = "abc456" };
+        Post post = new() { Id = 1234, Author = user };
+        ICollection<Model> store = [user, post];
+        using var transaction = new InMemoryCrudTransaction(store);
+        var result = transaction.Delete((Model[])[user2]);
+        Assert.That(result.IsSuccessful, Is.False);
+        Assert.That(result.Reason, Is.EqualTo(FailureReason.NotFound));
+    }
+    [Test]
+    public async Task Delete_with_generic_model_deletes_entities()
+    {
+        User user = new() { Id = "abc123" };
+        User user2 = new() { Id = "abc456" };
+        Post post = new() { Id = 1234, Author = user };
+        ICollection<Model> store = [user, post, user2];
+        using var transaction = new InMemoryCrudTransaction(store);
+        var result = transaction.Delete([user2]);
+        await transaction.CommitAsync();
+        Assert.That(result.IsSuccessful, Is.True);
+        Assert.That(store, Is.EquivalentTo((Model[])[user, post]));
+    }
+
+
+    [Test]
+    public void Delete_with_generic_model_fails_if_not_found()
+    {
+        User user = new() { Id = "abc123" };
+        User user2 = new() { Id = "abc456" };
+        Post post = new() { Id = 1234, Author = user };
+        ICollection<Model> store = [user, post];
+        using var transaction = new InMemoryCrudTransaction(store);
+        var result = transaction.Delete([user2]);
+        Assert.That(result.IsSuccessful, Is.False);
+        Assert.That(result.Reason, Is.EqualTo(FailureReason.NotFound));
+    }
+
+    [Test]
+    public void Discard_discards_changes()
+    {
+        var newUser = new User("CreateTest", "Test user");
+        ICollection<Model> store = [];
+        using (var transaction = new InMemoryCrudTransaction(store))
+        {
+            transaction.Create(newUser);
+            Assert.That(transaction.Discard().IsSuccessful, Is.True);
+        }
+        Assert.That(store, Is.Empty);
+    }
+
+    [Test]
+    public void Commit_does_nothing_if_no_chages()
+    {
+        ICollection<Model> store = [];
+        using (var transaction = new InMemoryCrudTransaction(store))
+        {
+            Assert.That(() => transaction.Commit(), Throws.Nothing);
+        }
         Assert.That(store, Is.Empty);
     }
 }
